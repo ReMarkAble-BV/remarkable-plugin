aa97a0e8e6f627bf53d6dce438c6aeaf210f8ea6
Marvin Linthorst <marvin.linthorst@gmail.com>
2021-10-13 08:47:52 +0200

    init

---
+++ remarkable-plugin.php
@@ -0,0 +1,24 @@
+<?php
+   /*
+   Plugin Name: ReMarkAble plug-in
+   Plugin URI: https://remarkable.nl
+   Description:
+   Version: 1.0.0
+   Author: ReMarkAble Communicatie B.V.
+   Author URI: https://remarkable.nl/
+   License: GPL3
+   */
+
+require 'plugin-update-checker/plugin-update-checker.php';
+
+$updateChecker = Puc_v4_Factory::buildUpdateChecker(
+    'https://github.com/ReMarkAble-BV/remarkable-plugin',
+    __FILE__,
+    'remarkable-plugin'
+);
+$updateChecker->getVcsApi()->enableReleaseAssets();
+$updateChecker->setAuthentication('your-token-here');
+
+
+add_filter( 'auto_plugin_update_send_email', '__return_false' );
+
